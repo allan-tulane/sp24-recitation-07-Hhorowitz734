eb36b11f83d41504b7b4d2d699be342563a1362e
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-04-17 18:28:32 -0500

    finished lab

--- answers.md
+++ answers.md
@@ -1,17 +1,16 @@
 # CMPS 2200 Recitation 10## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:** Benjamin Horowitz
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
 
 
 
-- **2)**
+- **2)** The work of reachable is O(m + n). You visit each node and each edge exactly once using my method, which is why the work is n (nodes) + m (edges).
 
-- **4)**
+- **4)** My function only calls reachable once, so this is the worst case.
 
-- **5)**
+- **5)** Connected really just calls reachable and then makes a comparison, so the work is the same as reachable. That is O(m + n).
 
-- **7)**
+- **7)** For an adjacency matrix, we would have to go through every entry in the matrix to determine connectedness. Therefore, it would be O(n^2). That is because instead of going over existing edges, we would also have to go over non-existing edges to figure it out.
--- main.py
+++ main.py
@@ -17,8 +17,17 @@ def reachable(graph, start_node):
     result = set([start_node])
     frontier = set([start_node])
     while len(frontier) != 0:
-        ###TODO
-        pass
+        
+        
+        curr = frontier.pop() #Curr - > current node we will operate on
+
+        for node in graph[curr]: #We look at the node's neighbors
+
+            if node not in result: #If we haven't already seen the node...
+                result.add(node) #We add it to the seen list
+                frontier.add(node) #And to the frontier
+    
+
     return result
 
 
@@ -26,8 +35,11 @@ def reachable(graph, start_node):
 
 
 def connected(graph):
-    ### TODO
-    pass
+    
+    start = next(iter(graph)) #Start from arbitrary node
+    reachednode = reachable(graph, start) 
+
+    return (len(reachednode)) == len(graph) #If the length of the graph is not the reached area, its not connected
 
 
 
@@ -37,6 +49,18 @@ def n_components(graph):
     Returns:
       the number of connected components in an undirected graph
     """
-    ### TODO
-    pass
+    
+    visited = set() #We need to keep track of visited nodes
+    count = 0 #Counts components
 
+    #This function is basically a combination of reachable and connected
+    #As far as the logic goes
+    for node in graph:
+
+        if node not in visited:
+
+            n_reachable = reachable(graph, node)
+            visited.update(n_reachable)
+            count += 1
+    
+    return count
